f71af14323304d6ef9a37f7c8c53ed34413f4030
Emmanuel Odenyire Anyira <eodenyire@gmail.com>
2024-04-27 08:26:19 -0700

    Skeletons for task 2

--- sort_task2.h
+++ sort_task2.h
@@ -20,7 +20,6 @@ typedef struct listint_s
 /* Function prototypes */
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
-void selection_sort(int *array, size_t size); /* Added selection_sort prototype */
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 
