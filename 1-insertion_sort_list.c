f13f54cf390ee584de4140537010c9f88a606ade
Emmanuel Odenyire Anyira <eodenyire@gmail.com>
2024-04-27 07:37:57 -0700

    Reseting a commit for some rechecks

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,46 +1,43 @@
-#include <stdio.h>
 #include "sort.h"
+#include <stdio.h>
 
 /**
  * insertion_sort_list - Sorts a doubly linked list of integers
  *                       in ascending order using the Insertion sort algorithm
  * @list: Pointer to the pointer to the head of the list
  */
-
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *ptr, *tmp;
+    listint_t *ptr, *tmp;
 
-	if (!list)
-		return;
+    if (!list || !(*list) || !((*list)->next))
+        return;
 
-	ptr = *list;
+    ptr = (*list)->next;
 
-	while (ptr)
-	{
-		while (ptr->next && (ptr->n > ptr->next->n))
-		{
-			tmp = ptr->next;
-			ptr->next = tmp->next;
-			tmp->prev = ptr->prev;
+    while (ptr)
+    {
+        tmp = ptr;
+        ptr = ptr->next;
 
-			if (ptr->prev)
-				ptr->prev->next = tmp;
+        while (tmp->prev && (tmp->n < tmp->prev->n))
+        {
+            if (tmp->next)
+                tmp->next->prev = tmp->prev;
 
-			if (tmp->next)
-				tmp->next->prev = ptr;
+            tmp->prev->next = tmp->next;
+            tmp->next = tmp->prev;
+            tmp->prev = tmp->prev->prev;
 
-			ptr->prev = tmp;
-			tmp->next = ptr;
+            if (tmp->prev)
+                tmp->prev->next = tmp;
+            else
+                *list = tmp;
 
-			if (tmp->prev)
-				ptr = tmp->prev;
-			else
-				*list = tmp;
+            if (tmp->next)
+                tmp->next->prev = tmp;
 
-			print_list(*list);
-		}
-		ptr = ptr->next;
-	}
+            print_list(*list);
+        }
+    }
 }
-
