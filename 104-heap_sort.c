7a010b4c7121ef57d19af771d333fef7b1cec320
Emmanuel Odenyire Anyira <eodenyire@gmail.com>
2024-04-29 14:09:36 -0700

    Final checks on the heapsort

--- 104-heap_sort.c
+++ 104-heap_sort.c
@@ -1,58 +1,70 @@
-#include <stdlib.h>
 #include "sort.h"
-
 /**
- * sift_down - Repair the heap whose root element is at index 'start'
- * @array: Array to be sorted
- * @start: Index of the root element of the heap
- * @end: Index of the last element in the array
- * @size: Size of the array
- */
-void sift_down(int *array, size_t start, size_t end, size_t size)
+ * check_tree - swiftdown check
+ * @array: pointer to array
+ * @size: size of the pointer
+ * @size_init: original size of the array
+ * @i: index as a root of the tree
+ *
+**/
+void check_tree(int *array, size_t size_init, size_t size, size_t i)
 {
-    size_t root = start;
-    size_t child;
-    int temp;
 
-    while (2 * root + 1 <= end)
-    {
-        child = 2 * root + 1;
-        if (child + 1 <= end && array[child] < array[child + 1])
-            child++;
-        if (array[root] < array[child])
-        {
-            temp = array[root];
-            array[root] = array[child];
-            array[child] = temp;
-            print_array(array, size);
-            root = child;
-        }
-        else
-            return;
-    }
+	int n, branch1, branch2;
+	size_t br1, br2;
+
+	br1 = i * 2 + 1;
+	br2 = br1 + 1;
+	branch1 = array[br1];
+	branch2 = array[br2];
+	if (((br1 < size) && (br2 < size) &&
+		(branch1 >= branch2 && branch1 > array[i]))
+		|| ((br1 == size - 1) && branch1 > array[i]))
+	{
+		n = array[i];
+		array[i] = branch1;
+		array[br1] = n;
+		print_array(array, size_init);
+	}
+	else if ((br1 < size) && (br2 < size) &&
+		(branch2 > branch1 && branch2 > array[i]))
+	{
+		n = array[i];
+		array[i] = branch2;
+		array[br2] = n;
+		print_array(array, size_init);
+	}
+	if (br1 < size - 1)
+		check_tree(array, size_init, size, br1);
+	if (br2 < size - 1)
+		check_tree(array, size_init, size, br2);
 }
-
 /**
- * heap_sort - Sorts an array of integers in ascending order using Heap sort algorithm
- * @array: Array to be sorted
- * @size: Size of the array
- */
+ * heap_sort - sorts an array of integers
+ * in ascending order using the Heap
+ * sort algorithm
+ * @array: pointer to array
+ * @size: size of the pointer
+ *
+**/
 void heap_sort(int *array, size_t size)
 {
-    int temp;
-    size_t end = size - 1;
-    size_t start = (size - 2) / 2;
-    size_t i;
+	size_t i, size_init = size;
+	int n;
 
-    for (i = start; i < size; i--)
-        sift_down(array, i, end, size);
-    for (i = end; i > 0; i--)
-    {
-        temp = array[0];
-        array[0] = array[i];
-        array[i] = temp;
-        print_array(array, size);
-        sift_down(array, 0, i - 1, size);
-    }
+	if (!array)
+		return;
+	for (i = 0; i < size / 2 ; i++)
+	{
+		check_tree(array, size_init, size, size / 2 - 1 - i);
+	}
+	for (i = 0; i < size_init - 1; i++)
+	{
+		n = array[0];
+		array[0] = array[size - 1 - i];
+		array[size - 1 - i] = n;
+		print_array(array, size_init);
+		check_tree(array, size_init, size - i - 1, 0);
+	}
+
 }
-
