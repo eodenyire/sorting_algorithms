328f1583866201a036d8472ece8e859acc883985
Emmanuel Odenyire Anyira <eodenyire@gmail.com>
2024-04-29 11:42:54 -0700

    Doing several checks

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,48 +1,32 @@
-/*
- * File_name: 2-selection_sort.c
- * Created: 15-NOVEMBER-2023
- * Author: UMUTONI Kevine(simplykevine)
- * Size: large
- * Project: 0x1B. C - Sorting algorithms & Big O
- * Status: not yet submitted.
- * Partner: UMUTONI Kevine (only)
- */
-
-
 #include "sort.h"
-
 /**
- * selection_sort - sorts an array of integers in ascending order using the
- * Selection sort algorithm
- * @array: The array to sort
- * @size: The size of the array
- * @l: The index of the left item.
- *
+ * selection_sort - function that sorts an array
+ *@size:  size of array
+ * @array: list with numbers
  */
-
-
 void selection_sort(int *array, size_t size)
 {
-	size_t i, j, min;
-	int swap;
+	size_t i, index;
+	int tmp, swap, flag = 0;
 
-	if (array == NULL || size < 2)
+	if (array == NULL)
 		return;
-
-	for (i = 0; i < size - 1; i++)
+	for (i = 0; i < size; i++)
 	{
-		min = i;
-		for (j = i + 1; j < size; j++)
+		tmp = i;
+		flag = 0;
+		for (index = i + 1; index < size; index++)
 		{
-			if (array[j] < array[min])
-				min = j;
+			if (array[tmp] > array[index])
+			{
+				tmp = index;
+				flag += 1;
+			}
 		}
-		if (i != min)
-		{
-			swap = array[i];
-			array[i] = array[min];
-			array[min] = swap;
+		swap = array[i];
+		array[i] = array[tmp];
+		array[tmp] = swap;
+		if (flag != 0)
 			print_array(array, size);
-		}
 	}
 }
