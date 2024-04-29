a8712e22980ac46aa3129e798e089b07fd1b7521
Emmanuel Odenyire Anyira <eodenyire@gmail.com>
2024-04-29 11:36:24 -0700

    Doing several checks

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,35 +1,47 @@
 #include "sort.h"
-#include <stdio.h>
 
 /**
- * selection_sort - Sorts an array of integers in ascending order using
- *                  the Selection sort algorithm
- * @array: The array to be sorted
- * @size: The size of the array
+ * selection_sort - Calls function
+ * @array: Array to be sorted
+ * @size: Size of array given
+ * Descrtiption: Function that sorts an array using the Selection
+ * sort algotrithm
+ * Return: 0
  */
 void selection_sort(int *array, size_t size)
 {
-	size_t i, j, min_idx;
-	int temp;
+	unsigned int i, j, min_idx;
 
-	if (array == NULL || size < 2)
+	if (!array)
 		return;
 
 	for (i = 0; i < size - 1; i++)
 	{
 		min_idx = i;
 		for (j = i + 1; j < size; j++)
-		{
 			if (array[j] < array[min_idx])
 				min_idx = j;
-		}
-		if (min_idx != i)
+
+		if (i != min_idx)
 		{
-			temp = array[i];
-			array[i] = array[min_idx];
-			array[min_idx] = temp;
+			swap_func(&array[min_idx], &array[i]);
 			print_array(array, size);
 		}
 	}
 }
 
+/**
+ * swap_func - Function that swaps two values
+ *
+ * @a: Fisrt value
+ * @b: Second value
+ * Return: 0
+ */
+void swap_func(int *a, int *b)
+{
+	int tmp;
+
+	tmp = *b;
+	*b = *a;
+	*a = tmp;
+}
