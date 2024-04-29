e2a2e399aa87744423f87fcdee9d900a3f1de853
Emmanuel Odenyire Anyira <eodenyire@gmail.com>
2024-04-29 12:55:14 -0700

    Doing several checks

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -1,46 +1,48 @@
 #include "sort.h"
-#include <stdio.h>
-/**
- * _swap - swaped 2 values.
- * @array: the array for swap him values.
- * @i: First index
- * @j: Second index
- * Return: Nothing
- */
-void _swap(int *array, int i, int j)
-{
-	int tmp;
 
-	if (array[i] != array[j])
-	{
-		tmp = array[i];
-		array[i] = array[j];
-		array[j] = tmp;
-	}
+/**
+ * get_max_gap - gets the a largest Knuth Sequence gap for this size
+ * @size: the size of the array
+ *
+ * Return: the gap size
+ */
+size_t get_max_gap(size_t size)
+{
+	size_t n;
+
+	n = 1;
+	while (n < size)
+		n = n * 3 + 1;
+	return ((n - 1) / 3);
 }
 
 /**
- * shell_sort - sort the list and print the changes
- * @array: The array to sort.
- * @size: Size of the array
- * Return: Nothing
+ * shell_sort - shell_sort
+ * @array: the integer array to sort
+ * @size: the size of the array
+ *
+ * Return: void
  */
 void shell_sort(int *array, size_t size)
 {
-	size_t h = 0,  i, j;
+	size_t gap, i, j;
+	int temp;
 
-	if (size < 2)
+	if (!array || !size)
 		return;
 
-	while (h <= size / 3)
-		h = h * 3 + 1;
 
-	while (h >= 1)
+	for (gap = get_max_gap(size); gap; gap = (gap - 1) / 3)
 	{
-		for (i = h; i < size; i++)
-			for (j = i; j >= h && array[j] < array[j - h]; j -= h)
-				_swap(array, j, j - h);
-		h /= 3;
+		for (i = gap; i < size; i++)
+		{
+			temp = array[i];
+			for (j = i; j > gap - 1 && array[j - gap] > temp; j -= gap)
+			{
+				array[j] = array[j - gap];
+			}
+			array[j] = temp;
+		}
 		print_array(array, size);
 	}
 }
