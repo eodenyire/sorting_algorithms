675fecc42039e8285527bba8c5655df22486bf5f
Emmanuel Odenyire Anyira <eodenyire@gmail.com>
2024-04-29 11:38:58 -0700

    Doing several checks

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,47 +1,48 @@
+/*
+ * File_name: 2-selection_sort.c
+ * Created: 15-NOVEMBER-2023
+ * Author: UMUTONI Kevine(simplykevine)
+ * Size: large
+ * Project: 0x1B. C - Sorting algorithms & Big O
+ * Status: not yet submitted.
+ * Partner: UMUTONI Kevine (only)
+ */
+
+
 #include "sort.h"
 
 /**
- * selection_sort - Calls function
- * @array: Array to be sorted
- * @size: Size of array given
- * Descrtiption: Function that sorts an array using the Selection
- * sort algotrithm
- * Return: 0
+ * selection_sort - sorts an array of integers in ascending order using the
+ * Selection sort algorithm
+ * @array: The array to sort
+ * @size: The size of the array
+ * @l: The index of the left item.
+ *
  */
+
+
 void selection_sort(int *array, size_t size)
 {
-	unsigned int i, j, min_idx;
+	size_t i, j, min;
+	int swap;
 
-	if (!array)
+	if (array == NULL || size < 2)
 		return;
 
 	for (i = 0; i < size - 1; i++)
 	{
-		min_idx = i;
+		min = i;
 		for (j = i + 1; j < size; j++)
-			if (array[j] < array[min_idx])
-				min_idx = j;
-
-		if (i != min_idx)
 		{
-			swap_func(&array[min_idx], &array[i]);
+			if (array[j] < array[min])
+				min = j;
+		}
+		if (i != min)
+		{
+			swap = array[i];
+			array[i] = array[min];
+			array[min] = swap;
 			print_array(array, size);
 		}
 	}
-}
-
-/**
- * swap_func - Function that swaps two values
- *
- * @a: Fisrt value
- * @b: Second value
- * Return: 0
- */
-void swap_func(int *a, int *b)
-{
-	int tmp;
-
-	tmp = *b;
-	*b = *a;
-	*a = tmp;
 }
