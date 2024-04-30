5e0c296a2bf408624b44de1780324123e3fdf21e
Emmanuel Odenyire Anyira <eodenyire@gmail.com>
2024-04-30 08:49:50 -0700

    A different logic for the bubble sort algorithm

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -2,34 +2,41 @@
 #include <stdio.h>
 
 /**
- * bubble_sort - sorts an array of integers in ascending order
+ * bubble_sort - sorts an array of integers in ascending order using the
+ *               Bubble Sort algorithm
  *
- * @array: The array of integer
- * @size: The size of the array
+ * @array: pointer to the array of integers to be sorted
+ * @size: number of elements in the array
+ *
+ * Description: This function sorts an array of integers in ascending order
+ *              using the Bubble Sort algorithm. It repeatedly steps through
+ *              the list, compares adjacent elements, and swaps them if they
+ *              are in the wrong order. This process is repeated until the
+ *              array is sorted.
  *
  * Return: void
  */
 void bubble_sort(int *array, size_t size)
 {
 	size_t i;
-	int tmp, sorted;
-
+	int temp, if_sorted;
+	/* Check if array is NULL or empty */
 	if (!array || size == 0)
 		return;
 
 	do {
-		sorted = 1;
+		if_sorted = 1;
 		for (i = 0; i < size - 1; i++)
 		{
 			if (array[i] > array[i + 1])
 			{
-				sorted = 0;
-				tmp = array[i];
+				if_sorted = 0;
+				temp = array[i];
 				array[i] = array[i + 1];
-				array[i + 1] = tmp;
+				array[i + 1] = temp;
 				print_array(array, size);
 			}
 		}
 
-	} while (sorted == 0);
+	} while (if_sorted == 0);
 }
--- 0-bubble_sort1.c
+++ 0-bubble_sort1.c
@@ -1,38 +1,35 @@
 #include "sort.h"
+#include <stdio.h>
 
 /**
- * bubble_sort - Sorts an array of integers in ascending order
- *               using the Bubble sort algorithm
- * @array: Array to be sorted
- * @size: Size of the array
+ * bubble_sort - sorts an array of integers in ascending order
+ *
+ * @array: The array of integer
+ * @size: The size of the array
+ *
+ * Return: void
  */
 void bubble_sort(int *array, size_t size)
 {
-	size_t i, j;
-	int temp;
-	int swapped;
+	size_t i;
+	int tmp, sorted;
 
-	if (!array || size < 2)
+	if (!array || size == 0)
 		return;
 
-	for (i = 0; i < size - 1; i++)
-	{
-		swapped = 0;
-		for (j = 0; j < size - i - 1; j++)
+	do {
+		sorted = 1;
+		for (i = 0; i < size - 1; i++)
 		{
-			if (array[j] > array[j + 1])
+			if (array[i] > array[i + 1])
 			{
-				/* Swap array[j] and array[j + 1] */
-				temp = array[j];
-				array[j] = array[j + 1];
-				array[j + 1] = temp;
-				swapped = 1;
+				sorted = 0;
+				tmp = array[i];
+				array[i] = array[i + 1];
+				array[i + 1] = tmp;
 				print_array(array, size);
 			}
 		}
-		/* If no two elements were swapped in the inner loop, then break */
-		if (swapped == 0)
-			break;
-	}
-}
 
+	} while (sorted == 0);
+}
